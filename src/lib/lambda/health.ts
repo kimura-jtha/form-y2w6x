082e98ddcb98b5128db12159b21aac02dd5b3a49
Kimura <249289934+kimura-jtha@users.noreply.github.com>
2026-01-12 14:29:06 +0700

    keep: 2026/01/12 14:29:06

--- src/lib/lambda/health.ts
+++ src/lib/lambda/health.ts
@@ -1,11 +1,12 @@
-import { asyncDeduplicator } from '../../utils/dedupe';
-import { fetchLambda } from './_helper';
+// import { asyncDeduplicator } from '../../utils/dedupe';
+// import { fetchLambda } from './_helper';
 export async function alive() {
-  return asyncDeduplicator.call('health', async () => {
-    const response = await fetchLambda<{ status: 'healthy' }>({
-      path: 'health',
-      method: 'GET',
-    });
-    return response.status === 'healthy';
-  });
+  return Promise.resolve(true);
+  // return asyncDeduplicator.call('health', async () => {
+  //   const response = await fetchLambda<{ status: 'healthy' }>({
+  //     path: 'health',
+  //     method: 'GET',
+  //   });
+  //   return response.status === 'healthy';
+  // });
 }
